092db476a7d7d6a3211f844b3c6c38f5ebd3d3e1
sam mbugua <sam@systech>
2021-09-24 17:00:35 +0300

    create a api

---
+++ src/main/java/com/infinitecovers/property/PropertyApplication.java
@@ -0,0 +1,66 @@
+package com.infinitecovers.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootApplication
+public class PropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyApplication.class, args);
+	}
+
+}
+
+class Coffee{
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id =id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Black"),
+				new Coffee("white"),
+				new Coffee("Red"),
+				new Coffee("Blue")
+		));
+	}
+
+	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+}
